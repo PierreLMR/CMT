031adace34b70e8ea95fad415f4957932e8032c1
Puck7 <pierre.lemaire.777@gmail.com>
2016-06-09 23:02:35 +0200

    .

--- app.js
+++ app.js
@@ -4,22 +4,7 @@ require('./models/Courses');
 
 var db;
 
-mongoose.connect(process.env.MONGODB_URI, function (err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  db = database;
-  console.log("Database connection ready");
-
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
-});
+mongoose.connect(process.env.MONGODB_URI);
  //mongodb://<dbuser>:<dbpassword>@ds055842.mlab.com:55842/heroku_6g8blllx
 
 
